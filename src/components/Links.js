bacebdc06e1334656f039720a06949fe45c15a89
gfundire <52905358+gfundire@users.noreply.github.com>
2023-03-22 18:07:35 +0200

    updates navigation

---
+++ src/components/Links.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Links = () => {
+  const links = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about-us' },
+    { name: 'Reservations', path: '/reservations' },
+    { name: 'Order Online', path: '/order-online' },
+    { name: 'Login', path: '/login' },
+  ];
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <li className="nav-item">
+            <Link to={link.path} className="nav-link">
+              {link.name}
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+};
+export default Links;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,38 @@
 import React from 'react';
+import Links from './Links';
+import { Link } from 'react-router-dom';
 
-const Nav = ()=>( 
+const Nav = () => (
   <nav className="navbar navbar-expand-md nav-pills fixed-top bg-light">
-      <div className="container">
-      <a className="navbar-brand" href="#">
-        <img className="d-inline-block" width="140" height="40" src="https://images2.imgbox.com/fe/61/RrOIGU8Y_o.png" alt="logo"/>
-      </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarCollapse"
-      aria-controls="navbarCollapse" aria-expanded="false"
-      aria-label="Toggle Navigation"
+    <div className="container">
+      <Link to="/" className="navbar-brand">
+        <img
+          className="d-inline-block"
+          width="140"
+          height="40"
+          src="https://images2.imgbox.com/fe/61/RrOIGU8Y_o.png"
+          alt="logo"
+        />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle Navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-        <ul className="collapse navbar-collapse nav navbar-nav" id="navbarCollapse">    
-
-          <li className="nav-item nav-fill">
-            <a className="nav-link active" href="#">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Reservations
-            </a>
-          </li>
-          <li className="nav-item ">
-            <a className="nav-link text-nowrap" href="#">
-              Order Online
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Login
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>  
-    
+      <ul
+        className="collapse navbar-collapse nav navbar-nav"
+        id="navbarCollapse"
+      >
+        <Links />
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Nav;
