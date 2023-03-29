decd02a51d318daec876ccfc9909eda7c4b14e29
gfundire <52905358+gfundire@users.noreply.github.com>
2023-03-29 16:46:21 +0200

    Styled CallToAction

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,24 +5,24 @@ import './call-to-action.css';
 const CallToAction = () => {
   return (
     <Section>
-      <div className="col-sm-6">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
+      <div className="col-sm-6 hero-text">
+        <h1 className="color-primary">Little Lemon</h1>
+        <h2 className="color-primary">Chicago</h2>
         <p>
           We are a family owned Mediterranean restaurant, focused on traditional
           recipes served with a modern twist.
         </p>
         <div>
-          <button className="btn btn-primary">Reserve a Table</button>
+          <button className="btn btn-primary round color-dark">Reserve a Table</button>
         </div>
       </div>
-      
-      <img
-        className="img col-sm-6 rounded"
-        src="https://images2.imgbox.com/81/3e/scEJEW4D_o.png"
-        alt="little lemon"
-      />
-      
+      <div className="col-sm-6">
+        <img
+          className="round"
+          src="https://images2.imgbox.com/81/3e/scEJEW4D_o.png"
+          alt="little lemon"
+        />
+      </div>
     </Section>
   );
 };
