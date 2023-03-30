981191aab2ec0d21c32d2876ae232d46c772938c
gfundire <52905358+gfundire@users.noreply.github.com>
2023-03-30 19:27:53 +0200

    Adds flexbox to specials section

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 import dishes from '../utils/dishes';
 import Card from './Card';
-
+import './specials.css';
 
 const Specials = () => {
   return (
@@ -15,12 +15,16 @@ const Specials = () => {
           <Button className="float-end">Online Menu</Button>
         </div>
       </div>
-      <div className="card-area pb-5">     
-          {dishes.map((d) => (
-           
-            <Card key={d.name} title={d.name} text={d.description} src={d.src} price={d.price}/>
-            
-          ))}      
+      <div className="card-area pb-5">
+        {dishes.map((d) => (
+          <Card
+            key={d.name}
+            title={d.name}
+            text={d.description}
+            src={d.src}
+            price={d.price}
+          />
+        ))}
       </div>
     </section>
   );
