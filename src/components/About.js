62f7523052f5c436123a50b4c5208b4289aada17
gfundire <52905358+gfundire@users.noreply.github.com>
2023-03-31 16:59:29 +0200

    adds about.css

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./about.css";
 
 const About = () => {
   return (
-    <section className="row">
-      <article className="col-md-6">
+    <section className="row mb-5">
+      <article className="col-md-6 about-section">
         <p>
           Little Lemon is a charming neighbourhood bistro that serves a simple
           food and classic cocktails in a lively but casual environment.
@@ -15,6 +16,7 @@ const About = () => {
       </article>
       <article className="col-md-6">
         <img
+          className="img-fluid rounded"
           src="https://thumbs2.imgbox.com/b7/97/VqONgn8H_t.png"
           alt="Adrian and Mario"
         />
