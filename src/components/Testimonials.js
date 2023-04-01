bcf5ea989669c6b4c48e44571d34a6b0bb980d6c
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-01 17:36:53 +0200

    fixes testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,83 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './testimonials.css';
 
 const Testimonials = () => {
-  return (
-    <section >
-      <div id="carouselExampleCaptions" className="carousel slide">
-        <div className ="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div className ="carousel-inner">
-          <div className ="carousel-item active">
-            <img  src="https://placehold.jp/30/495e57/495e57/500x200.png" className ="d-block w-100" alt="..." />
-            <div className ="carousel-caption d-none d-sm-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
-          </div>
-          <div className ="carousel-item">
-            <img src="https://placehold.jp/30/495e57/495e57/500x200.png"  alt="..." />
-            <div className ="carousel-caption d-none d-sm-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </div>
-          <div className ="carousel-item">
-            <img src="https://placehold.jp/30/495e57/495e57/500x200.png"  className ="d-block w-100" alt="..." />
-            <div className ="carousel-caption d-none d-sm-block">
-              
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-      
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://randomuser.me/api/?results=3');
+      const response = await res.json();
+      return response.results;
+    };
+    const profiles = fetchData();
+    setData(profiles);
+  }, []);
+  return data.length ? (
+    <section className="row my-3">
+      <article className="col-md-4 bg-light">Article 1</article>
+      <article className="col-md-4 bg-light">Article 1</article>
+      <article className="col-md-4 bg-light">Article 1</article>
     </section>
+  ) : (
+    <div class="alert alert-info" role="alert">
+      No testimonials at the moment
+    </div>
   );
 };
 
