b066ea07142e34308d12ddf9574870619b5e80a2
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-02 16:06:30 +0200

    Adds Carousel to reservations

---
+++ src/components/Corousel.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const Corousel = () => {
+  return (
+    <section id="carouselExampleCaptions" className="carousel slide">
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://thumbs2.imgbox.com/75/4e/tax9MvIQ_t.jpg" className="d-block w-100" alt="little Lemon" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="Little Lemon" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="Little Lemon" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </section>
+  );
+};
+
+export default Corousel;
+
+
+<a href="http://imgbox.com/tax9MvIQ" target="_blank"><img src="https://thumbs2.imgbox.com/75/4e/tax9MvIQ_t.jpg" alt="imgbox"/></a>
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Corousel from "./Corousel";
 
 const Reservations = () => {
   return (
-    <>
-      <div className="mt-5 pb-5">
-        <h1 className="display-1">Reserve Your Table Here</h1>
-        <h1 className="h1 mt-5">Reserve Your Table Here</h1>
-        <h1 className="mt-5 display-1">Reserve Your Table Here</h1>
-        <h1 className="h6 mt-5">Reserve Your Table Here</h1>
-      </div>
-    </>
+    <section>
+      <Corousel/>
+      <article>
+        <form>
+
+        </form>
+      </article>
+      
+    </section>
   );
 };
 
