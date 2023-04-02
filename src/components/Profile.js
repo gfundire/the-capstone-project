30138b1e9d5ad944014dec75209e8a6ba3b90d60
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-02 15:28:09 +0200

    updates profile names

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './profile.css';
 
-const Profile = ({ src, name}) => {
+const Profile = ({ src, first, last }) => {
   return (
     <article className="col-md-4 profile">
       <div className="profile text-center">
         <img src={src} alt="testimonial" />
-        <h5 className="text-center">{name}</h5>
+        <h5 className="text-center">{first + ' ' + last}</h5>
         <p className="text-center">Rating</p>
-        <p>I Love Little Lemon. Their service is the best and their recipe is second to none</p>
+        <p>
+          I Love Little Lemon. Their service is the best and their recipe is
+          second to none
+        </p>
       </div>
     </article>
   );
