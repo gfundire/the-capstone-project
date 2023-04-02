3595eec3c741b646e3f2b2b994cd0198a258b1ac
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-02 09:03:20 +0200

    Adds useProfiles.js and modifed Testimonials.js

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,24 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useProfiles from '../hooks/useProfiles.js';
 import './testimonials.css';
 
 const Testimonials = () => {
-  const [profiles, setProfiles] = useState([]);
-  useEffect(() => {
-    const fetchData = () => {
-      fetch('https://randomuser.me/api/?results=3')
-        .then((res) => res.json())
-        .then((res) => setProfiles(res.results))
-        .catch((error) => console.log(error));
-    };
-    fetchData();
-  }, []);
-
+  const { profiles } = useProfiles();
+  
   if (profiles.length > 0) {
     return (
       <section className="row my-3">
-        <article className="col-md-4 bg-light"><img src={profiles[0].picture.large} /></article>
-        <article className="col-md-4 bg-light"><img src={profiles[1].picture.large} /></article>
-        <article className="col-md-4 bg-light"><img src={profiles[2].picture.large} /></article>
+        <article className="col-md-4 bg-light">
+          <img src={profiles[0].picture.large} />
+        </article>
+        <article className="col-md-4 bg-light">
+          <img src={profiles[1].picture.large} />
+        </article>
+        <article className="col-md-4 bg-light">
+          <img src={profiles[2].picture.large} />
+        </article>
       </section>
     );
   }
---
+++ src/hooks/useProfiles.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useProfiles = () => {
+  const [profiles, setProfiles] = useState([]);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://randomuser.me/api/?results=3')
+        .then((res) => res.json())
+        .then((res) => setProfiles(res.results))
+        .catch((error) => console.log(error));
+    };
+    fetchData();
+  }, []);
+  return { profiles };
+};
+
+export default useProfiles;
