629ac55919b01749f546c73451ab65ea6f33a14c
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-03 20:20:32 +0200

    changes reservations

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Corousel from './Corousel';
-import BookingForm from './BookingForm';
-// import DetailsForm from './DetailsForm';
+import DetailsForm from './DetailsForm';
 
 const Reservations = () => {
   return (
     <section>
       <Corousel />
-      <article className="container">
-        <Routes>          
-          <Route path="/reservations/booking" element={<BookingForm />}></Route>
-        </Routes>
-        {/* <DetailsForm /> */}
+      <article className="container">        
+        <DetailsForm />
       </article>
     </section>
   );
