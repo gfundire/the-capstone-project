7df3d160a5a4e476e4077425dbe67051151ea173
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-04 16:54:38 +0200

    adds useReducer

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -4,18 +4,19 @@ import BookingForm from './BookingForm';
 import DetailsForm from './DetailsForm';
 
 const Reservations = () => {
-  const reducer = (state, action) => {
-    if (action.type === 'book') return { available: state.available };
-    return state;
-  };
-  const [availableTimes] = useState([
+  const initialState = [
     '17:00',
     '18:00',
     '19:00',
     '20:00',
     '21:00',
     '22:00',
-  ]);
+  ];
+  const updateTimes = (state, action) => {    
+    return state;
+  };
+  const initializeTimes = ()=>initialState;
+  const [availableTimes,dispatch] = useReducer(updateTimes,initializeTimes());
 
   const handleChange = (e) => {
     setAvailableTimes(e.target.value);
