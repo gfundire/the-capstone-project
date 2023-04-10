ba0536ce5501accdaededecd1c315f99eb66d287
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-10 15:34:58 +0200

    adds confirmation.css

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import Corousel from "./Corousel";
+import Corousel from './Corousel';
+import './confirmation.css';
 
-const ConfirmedBooking = ()=>{
-  <section>
+const ConfirmedBooking = () => {
+  return (
+    <section>
       <Corousel />
-      <article className="container">
-      <h1>Success! <span><i class="bi bi-check-circle"></i></span></h1>
-      <p>Your Table Reservation was successful, your invoice was sent via your email address.</p>  
-      <p>Thank you for choosing us</p>          
+      <article className="container success pt-5">
+        <h1 className="my-5 text-center">
+          Success!{' '}
+          <span>
+            <i class="bi bi-check-circle"></i>
+          </span>
+        </h1>
+        <p className="my-0">
+          Your Table Reservation was successful, your invoice was sent via your
+          email address.
+        </p>
+        <p >Thank you for choosing us.</p>
       </article>
     </section>
-
+  );
 };
 
 export default ConfirmedBooking;
--- src/utils/siteLinks.js
+++ src/utils/siteLinks.js
@@ -2,6 +2,7 @@ const siteLinks = [
   { name: 'Home', path: '/' },
   { name: 'About', path: '/about-us' },
   { name: 'Reservations', path: '/reservations' },
+  { name: 'Confirmation', path: '/confirmation' },
   { name: 'Order Online', path: '/order-online' },
   { name: 'Login', path: '/login' },
 ];
