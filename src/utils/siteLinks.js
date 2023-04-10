723f6bf5b142a023bc3940359c059178e2eb4ed3
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-10 18:02:12 +0200

    fixes siteLinks

--- src/utils/siteLinks.js
+++ src/utils/siteLinks.js
@@ -1,7 +1,7 @@
 const siteLinks = [
   { name: 'Home', path: '/' },
   { name: 'About', path: '/about-us' },
-  { name: 'Reservations', path: '/booking' },
+  { name: 'Reservations', path: '/bookings' },
   { name: 'Confirmation', path: '/confirmation' },
   { name: 'Order Online', path: '/order-online' },
   { name: 'Login', path: '/login' },
