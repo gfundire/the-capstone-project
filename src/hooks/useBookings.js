049a1e93c5379a661deeee4f3e1b4006419cc044
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-13 18:20:10 +0200

    adds yup

--- src/hooks/useBookings.js
+++ src/hooks/useBookings.js
@@ -1,6 +1,19 @@
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export default function useBooking() {
+  const schema = yup.object().shape({
+    res_date: yup.date().required("Required"),
+    res_time: yup.string().required("Required"),
+    guests: yup.number().positive().integer().required("Required"),
+    occasion:  yup.string().required("Required"),
+    seatingOption: yup.string().required("Required"),
+    firstName: yup.string().min(3).max(50).required("Required"),
+    lastName:  yup.string().min(3).max(50).required("Required"),
+    phoneNumber: yup.string().required("Required"),
+    email: yup.string().email().required("Required")
+  });
+
   const formik = useFormik({
     initialValues: {
       res_date: '',
@@ -14,6 +27,7 @@ export default function useBooking() {
       email: '',
       request: '',
     },
+    validationSchema:schema
   });
 
   return { formik };
