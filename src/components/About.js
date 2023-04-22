ff73075dd1d1e2cda0d454fc18670cbece27e88b
gfundire <52905358+gfundire@users.noreply.github.com>
2023-04-22 17:30:56 +0200

    updates About.js styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import "./about.css";
+import './about.css';
 
 const About = () => {
   return (
-    <section className="row py-5">
-      <article className="col-md-6 about-section">
-        <p>
-          Little Lemon is a charming neighbourhood bistro that serves a simple
-          food and classic cocktails in a lively but casual environment.
-        </p>
-        <p>
-          Co-owned by Adrian and Mario, the restaurant features a locally
-          sourced menu with daily specials.
-        </p>
-      </article>
-      <article className="col-md-6">
-        <img
-          className="img-fluid rounded"
-          src="https://thumbs2.imgbox.com/b7/97/VqONgn8H_t.png"
-          alt="Adrian and Mario"
-        />
-      </article>
+    <section className="about py-5">
+      <div className="container">
+        <div className="row">
+          <article className="col-md-6 about-section">
+            <p>
+              Little Lemon is a charming neighbourhood bistro that serves a
+              simple food and classic cocktails in a lively but casual
+              environment.
+            </p>
+            <p>
+              Co-owned by Adrian and Mario, the restaurant features a locally
+              sourced menu with daily specials.
+            </p>
+          </article>
+          <article className="col-md-6">
+            <img
+              className="img-fluid rounded"
+              src="https://thumbs2.imgbox.com/b7/97/VqONgn8H_t.png"
+              alt="Adrian and Mario"
+            />
+          </article>
+        </div>
+      </div>
     </section>
   );
 };
